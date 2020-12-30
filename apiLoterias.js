a16590bc62fb575404b70a621994ed288ebfd91e
Alex raul <alexbraul.ar@gmail.com>
2020-12-29 21:38:23 -0300

    second commit

--- apiLoterias.js
+++ apiLoterias.js
@@ -1,13 +1,27 @@
 const axios = require("axios");
-var url =
-  "https://apiloterias.com.br/app/resultado?loteria=megasena&token=tc5HiJvzOIoxRzq&concurso";
+const fs = require('fs');
+var d = fs.readFileSync('\data.txt').toString()
+var data = []
+// var url ="https://apiloterias.com.br/app/resultado?loteria=megasena&token=tc5HiJvzOIoxRzq&concurso";
 async function search() {
   var t;
-  for (a = 0; a <= 10; a++) {
-    t = await axios.get(
+  for (a = 0000; a <= 2329; a++) {
+    await axios.get(
       `https://apiloterias.com.br/app/resultado?loteria=megasena&token=tc5HiJvzOIoxRzq&concurso=${a}`
-    );
-    console.log(t.data.numero_concurso);
+    )
+      .then(res => {
+        if (res.data.dezenas) {
+          data.push(res.data.dezenas)
+        } else {
+          return
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    var text = data.map(e => `[${e.toString()}]`)
+    fs.writeFileSync('\data.txt', `${text.toString()}`)
   }
+
 }
 search();
